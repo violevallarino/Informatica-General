f0ab863daf57ee0d65e40b3051470515da91d219
violevallarino <violevallarino@gmail.com>
2026-05-21 22:34:07 -0300

    practica16

---
+++ practica-16/js/script.js
@@ -0,0 +1,14 @@
+let nombre = "Violeta"; 
+let apellido = "Vallarino";
+let nombreCompleto = nombre + ' ' + apellido;
+let edad = 18;
+const universidad = 'UNA';
+nombre = "vIOLETA"
+apellido = "dias"
+edad = 90
+
+console.log("nuevoNombre: " + nombre);
+console.log("nuevoApellido: " + apellido);
+console.log("nuevoNombre Completo: " + nombreCompleto);
+console.log("nuevaEdad: " + edad);
+console.log("nuevaUniversidad: " + universidad);
